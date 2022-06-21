fd0b441147c09776df95110bdc886d8eb2de297c
paolaxjean <pr1008631@gmail.com>
2022-06-20 23:19:09 -0700

    updated user.js

--- models/User.js
+++ models/User.js
@@ -1,44 +1,51 @@
 const { Schema, model } = require('mongoose');
 
+// Schema to create User model
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // the regex for valid email is cited from https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address'],
+            // another regex for matching an Email: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!'],
-    },
-    thoughts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Thought',
-      },
-    ],
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    ],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
 );
 
-userSchema.virtual('friendCount').get(function () {
-  return this.friends.length;
-});
-
-const User = model('User', userSchema);
+// Create a virtual called "friendCount" that retrieves the length of the user's friends array field on query
+userSchema
+    .virtual('friendCount')
+    .get(function totalFriends () {
+        return this.friends.length;
+    });
+  
+// Initialize our User model
+const User = model('user', userSchema);
 
 module.exports = User;
