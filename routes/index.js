6b47c231ab57ff1d60b02ff7a2b1b69479ad53f3
paolaxjean <pr1008631@gmail.com>
2022-06-20 23:27:02 -0700

    added code to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-router.use('/api', apiRoutes);
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-router.use((req, res) => {
-  return res.send('Wrong route!');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server is running on port ${PORT}!`);
+  });
 });
-
-module.exports = router;
