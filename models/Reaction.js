37c7d4efeb1d20233ec05f9feb9dd88adc9bd512
paolaxjean <pr1008631@gmail.com>
2022-06-20 23:23:08 -0700

    updated reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,33 +1,30 @@
-const { Schema, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const {Schema, Types} = require('mongoose');
 
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+const formatDate = (date) => {
+    return date.toLocaleDateString();
+};
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate,
+        },
     }
-  },
-  {
-    toJSON: {
-      getters: true
-    },
-    id: false
-  }
 );
 
 module.exports = reactionSchema;
